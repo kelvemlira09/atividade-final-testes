66adf0b66af69bed4a401e32ac5e3e9d8a132196
kelvemlira09 <74730442+kelvemlira09@users.noreply.github.com>
2022-10-03 19:16:04 -0300

    testes unitarios

--- src/test/java/com/example/requisicaoemprestimo/unitarios/EmprestimoTests.java
+++ src/test/java/com/example/requisicaoemprestimo/unitarios/EmprestimoTests.java
@@ -1,53 +1,48 @@
 package com.example.requisicaoemprestimo.unitarios;
 
+import com.example.requisicaoemprestimo.domain.models.Emprestimo;
+import com.example.requisicaoemprestimo.domain.models.Parcela;
+import com.example.requisicaoemprestimo.domain.models.ResultadoAnalise;
+import com.example.requisicaoemprestimo.domain.models.ResultadoTesouraria;
 import org.junit.jupiter.api.Test;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.UUID;
+
+import static com.example.requisicaoemprestimo.unitarios.EmprestimoTestsFixture.buildResultadoAnalise;
+import static com.example.requisicaoemprestimo.unitarios.EmprestimoTestsFixture.buildResultadoTesouraria;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EmprestimoTests {
+
+    EmprestimoTestsFixture fixture;
     @Test
-    public void testParcelas(){
-        // ARRANGE
-        //TODO: FAÇA USO DO FIXTURE
-        //TODO: Crie uma classe Emprestimo com valor 100 e quantidade de parcelas 12
-
-        // ACT
-        //TODO: Recupere as parcelas do emprestimo
-
-        // ASSERTS
-        //TODO: Validar se existe parcelas
-        //TODO: Validar o valor total de empréstimo é 106.50
-        //TODO: Validar se o número de parcelas é 12
+    public void testParcelas() {
+        Emprestimo emprestimo = fixture.emprestimoAprovado(100, 12);
+        Optional<Parcela[]> p = emprestimo.getParcelas();
+        assertTrue(emprestimo.getParcelas().isPresent());
+        assertEquals(106.50, emprestimo.getValorTotalEmprestimo());
+        assertEquals(12, emprestimo.getQuantidadeParcelasSolicitadas());
     }
-
     @Test
     public void testeAnaliseDeCreditoInvalida(){
-        // ARRANGE
-        //TODO: FAÇA USO DO FIXTURE
-        //TODO: Crie uma classe Emprestimo com valor 100 e quantidade de parcelas 12
+        Emprestimo emprestimo = fixture.emprestimoAprovado(100,12);
+        String[] analise = {"Analise Reprovada"};
+        ResultadoAnalise resultado = buildResultadoAnalise(false, analise);
+        emprestimo.setResultadoAnalise(resultado);
+        assertEquals(false, emprestimo.isEmprestimoFoiAprovado());
 
-        // ACT
-        //TODO: Crie uma análise de crédito rejeitando a proposta
-        //TODO: Associe or resulado ao emprestimo
-
-        // ASSERTS
-        //TODO: Validar que o  emprestimo NÃO está aprovado
     }
 
     @Test
     public void testeResultadoDaTesourariaInvalida(){
-        // ARRANGE
-        //TODO: FAÇA USO DO FIXTURE
-        //TODO: Crie uma classe Emprestimo com valor 100 e quantidade de parcelas 12
+        Emprestimo emprestimo = fixture.emprestimoAprovado(100,12);
+        ResultadoTesouraria resultado = buildResultadoTesouraria(false,"limite insuficiente", emprestimo);
+        emprestimo.setResultadoTesouraria(resultado);
+        assertEquals(false, emprestimo.isEmprestimoFoiAprovado());
 
-        // ACT
-        //TODO: Crie uma solicitação para tesouraria rejeitando a proposta
-        //TODO: Associe o resultado ao emprestimo
-
-        // ASSERTS
-        //TODO: Validar que o  emprestimo NÃO está aprovado
     }
 
 }
